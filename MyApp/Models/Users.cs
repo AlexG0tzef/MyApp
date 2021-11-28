b31f16343a9f739ef3a434b88047763dfcbecb72
vaskovskii.alexandr@gmail.com <vaskovskii.alexandr@gmail.com>
2021-11-28 16:39:15 +0300

    Добавьте файлы проекта.

---
+++ MyApp/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    //[Table("User")]
+    public class User
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Organization { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ MyApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public List<User> DatabaseUsers { get; set; }
+
+        public void Create()
+        {
+
+            using (DataContext context = new DataContext())
+            {
+                var name = NameTextBox.Text;
+                var address = OrganizationTextBox.Text;
+                var phonenumber = PhoneNumberTextBox.Text;
+
+                if (name != null && address != null && phonenumber != null)
+                {
+                    context.Users.Add(new User() { Name = name, Organization = address, PhoneNumber = phonenumber });
+                    context.SaveChanges();
+                }
+
+            }
+        }
+
+        public void Read()
+        {
+            using (DataContext context = new DataContext())
+            {
+                DatabaseUsers = context.Users.ToList();
+                ItemList.ItemsSource = DatabaseUsers;
+            }
+
+        }
+
+        public void Update()
+        {
+
+
+            using (DataContext context = new DataContext())
+            {
+
+                User selectedUser = ItemList.SelectedItem as User;
+
+                var name = NameTextBox.Text;
+                var organization = OrganizationTextBox.Text;
+                var phonenumber = PhoneNumberTextBox.Text;
+
+
+                if (name != null || organization != null)
+                {
+
+                    User user = context.Users.Find(selectedUser.Id);
+                    user.Name = name;
+                    user.Organization = organization;
+                    user.PhoneNumber = phonenumber;
+
+                    context.SaveChanges();
+                }
+
+            }
+
+
+
+        }
+        public void Delete()
+        {
+            using (DataContext context = new DataContext())
+            {
+                User selectedUser = ItemList.SelectedItem as User;
+
+                if (selectedUser != null)
+                {
+                    User user = context.Users.Single(x => x.Id == selectedUser.Id);
+
+                    context.Remove(user);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void ReadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Read();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Delete();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+            ItemList.Items.Clear();
+
+        }
+    }
+}
